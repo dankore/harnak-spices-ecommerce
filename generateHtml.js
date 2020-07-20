1aed6500e88a478e50802ddeb767e308f678372b
Adamu Muhammad Dankore <zimmazone@yahoo.com>
2020-07-20 07:37:29 +0000

    Webpack config and tailwind

---
+++ generateHtml.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import ReactDOMServer from 'react-dom/server';
+import fs from 'fs';
+
+
+function RENDER_THIS_HTML() {
+  return (
+    <div>
+        Hello from generate html
+    </div>
+  );
+}
+
+function html(x) {
+  return `<!DOCTYPE html>
+  <html lang="en">
+    <head>
+      <meta charset="UTF-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+      <title>Harnak Spices</title>
+      <link rel="stylesheet" href="/css/main.css" />
+      <script defer src="https://use.fontawesome.com/releases/v5.5.0/js/all.js"
+        integrity="sha384-GqVMZRt5Gn7tB9D9q7ONtcp4gtHIUEW/yG7h98J7IpE3kpi+srfFyyB/04OV6pG0" crossorigin="anonymous">
+      </script>
+    </head>
+    <body style="background-color: #f0f2f5; -webkit-font-smoothing: antialiased;">
+      <div id="app">
+      ${x}
+      </div>
+    </body>
+  </html>
+  `;
+}
+
+
+const reactHtml = ReactDOMServer.renderToString(<RENDER_THIS_HTML />);
+
+
+const overallHtmlString = html(reactHtml);
+
+
+const fileName = './app/index-template.html';
+const stream = fs.createWriteStream(fileName);
+stream.once('open', () => {
+  stream.end(overallHtmlString);
+});
---
+++ webpack.config.js
@@ -0,0 +1,77 @@
+const currentTask = process.env.npm_lifecycle_event;
+const path = require('path');
+const Dotenv = require('dotenv-webpack');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackHarddiskPlugin = require('html-webpack-harddisk-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const fse = require('fs-extra');
+
+/*
+  Because I didn't bother making CSS part of our
+  webpack workflow for this project I'm just
+  manually copying the CSS file to the DIST folder. 
+*/
+class RunAfterCompile {
+  apply(compiler) {
+    compiler.hooks.done.tap('Copy files', function () {
+      fse.copySync('./app/css/main.css', './dist/css/main.css');
+    });
+  }
+}
+
+config = {
+  entry: './app/Main.js',
+  output: {
+    publicPath: '/',
+    path: path.resolve(__dirname, 'app'),
+    filename: 'bundled.js',
+  },
+  plugins: [
+    new Dotenv(),
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: 'app/index-template.html',
+      alwaysWriteToDisk: true,
+    }),
+    new HtmlWebpackHarddiskPlugin(),
+  ],
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-react', ['@babel/preset-env', { targets: { node: '12' } }]],
+          },
+        },
+      },
+      { test: /\.css$/, use: 'css-loader' },
+    ],
+  },
+};
+
+if (currentTask == 'webpackDev' || currentTask == 'dev') {
+  config.devtool = 'source-map';
+  config.devServer = {
+    port: 3000,
+    contentBase: path.join(__dirname, 'app'),
+    hot: true,
+    historyApiFallback: { index: 'index.html' },
+  };
+}
+
+if (currentTask == 'webpackBuild') {
+  config.plugins.push(new CleanWebpackPlugin(), new RunAfterCompile());
+  config.mode = 'production';
+  config.output = {
+    publicPath: '/',
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].[chunkhash].js',
+    chunkFilename: '[name].[chunkhash].js',
+  };
+}
+
+module.exports = config;
