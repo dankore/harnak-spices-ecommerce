a34e420156ea75873dfa7096ece3447f57488a93
Adamu Muhammad Dankore <zimmazone@yahoo.com>
2020-07-20 10:31:43 +0000

    Update file

--- app/components/shared/Footer.js
+++ app/components/shared/Footer.js
@@ -2,9 +2,11 @@ import React from "react";
 
 function Footer() {
   return (
-    <div>
-      Harnak Spices @{new Date().getFullYear()}{" "}
-      <p>Web design by Adamu M. Dankore </p>
+    <div className="w-full flex justify-center">
+      <div className="text-center">
+        <p>Harnak Spices @{new Date().getFullYear()}</p>
+        <p className="text-gray-500 text-sm">Web design by Adamu M. Dankore </p>
+      </div>
     </div>
   );
 }
--- app/components/shared/Header.js
+++ app/components/shared/Header.js
@@ -2,10 +2,13 @@ import React from "react";
 
 function Header() {
   return (
-    <div>
-      <div className="w-full bg-white shaodow-xl flex justify-between px-4">
+    <div className="w-full bg-white">
+      <div className="max-w-2xl mx-auto shaodow-xl flex justify-between items-center px-4">
         <div>Harnak Spices</div>
-        <div>Login</div>
+        <div className="flex items-center">
+          <div className="mr-3">Login</div>
+          <div className="bg-black px-3 py-1 text-white">Sign Up</div>
+        </div>
       </div>
     </div>
   );
