454f575d05b4ee3a2e0d0448623cbb69aa074c45
Adamu Muhammad Dankore <zimmazone@yahoo.com>
2020-07-20 10:06:31 +0000

    Update file

--- app/Main.js
+++ app/Main.js
@@ -3,10 +3,12 @@ import ReactDOM from "react-dom";
 
 //COMPONENTS
 import Footer from "./components/shared/Footer";
+import Header from "./components/shared/Header";
 
 function Main() {
   return (
     <div>
+      <Header />
       Hi from main
       <Footer />
     </div>
--- app/components/shared/Header.js
+++ app/components/shared/Header.js
@@ -3,7 +3,10 @@ import React from "react";
 function Header() {
   return (
     <div>
-      <div>Harnak Spices</div>
+      <div className="w-full bg-white shaodow-xl flex justify-between px-4">
+        <div>Harnak Spices</div>
+        <div>Login</div>
+      </div>
     </div>
   );
 }
