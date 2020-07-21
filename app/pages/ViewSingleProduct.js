088de481cafea7655e79d2235db557e278ebb0c7
Adamu Muhammad Dankore <zimmazone@yahoo.com>
2020-07-21 10:12:26 +0000

    Update file

--- app/pages/ViewSingleProduct.js
+++ app/pages/ViewSingleProduct.js
@@ -46,30 +46,36 @@ function ViewSingleProduct() {
 
   return (
     <Page title="Harnak Spices: Online Shopping for Spices">
-      <h1 className="py-10 text-center">VIEW SINGLE</h1>
-      {/* SINGLE PRODCTU HTML */}
-      <div className="grid grid-cols-1 lg:grid-cols-2 max-w-lg">
-        <div className="flex justify-center">
+      <div>
+        <h1 className="pt-10 pb-3 text-2xl text-center">
+          {/* TITLE */}
+          {singleProduct.feed.title}
+        </h1>
+      </div>
+
+      {/* SINGLE PRODUCT HTML */}
+      <div className="grid grid-cols-1 lg:grid-cols-2 max-w-lg mx-auto">
+        <div>
           <img className="w-56 h-56 mb-2" src={singleProduct.feed.image} />
         </div>
-        <div className="">
-          {/* PRICE */}
-          <div className="flex justify-left">
-            <div>
-              <span className="text-red-600 block">
-                <span className="text-sm">Sale</span>{" "}
-                <span className="text-2xl font-bold">
-                  {singleProduct.feed.price}
+
+        <div className="flex items-center">
+          <div>
+            {/* PRICE */}
+            <div className="flex justify-left">
+              <div>
+                <span className="text-red-600 block">
+                  <span className="text-sm">Sale</span>{" "}
+                  <span className="text-2xl font-bold">
+                    {singleProduct.feed.price}
+                  </span>
                 </span>
-              </span>
-              <span className="text-sm text-gray-600 -mt-2 block">
-                Original: {(singleProduct.feed.price + 69).toFixed(2)}
-              </span>
+                <span className="text-sm text-gray-600 -mt-2 block">
+                  Original: {(singleProduct.feed.price + 69).toFixed(2)}
+                </span>
+              </div>
             </div>
           </div>
-
-          {/* TITLE */}
-          <p className="text-sm semi-bold mt-3">{singleProduct.feed.title}</p>
         </div>
       </div>
     </Page>
