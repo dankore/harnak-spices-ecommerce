f5ec2a185fec18a11227f900f42ddbc8dcb8ece0
Adamu Muhammad Dankore <zimmazone@yahoo.com>
2020-07-21 05:02:52 +0000

    Products page

---
+++ app/components/shared/Products.js
@@ -0,0 +1,48 @@
+import React from "react";
+import PropTypes from "prop-types";
+import Page from "./Page";
+import { Link } from "react-router-dom";
+
+function Products({ products }) {
+  //   {
+  //   "id": 1,
+  //   "title": "Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops",
+  //   "price": 109.95,
+  //   "description": "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
+  //   "category": "men clothing",
+  //   "image": "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg"
+  // }
+
+  return (
+    <Page title="Products">
+      <div className="bg-white flex justify-around flex-wrap">
+        {products.map((product) => {
+          return (
+            <Link to={"#"} className="w-56 my-10 lg:m-3" key={product.id}>
+              <img className="w-56 h-56 mb-2" src={product.image} />
+
+              {/* PRICE */}
+              <div className="flex justify-center">
+                <div>
+                  <span className="text-red-600 block">
+                    Sale: {product.price}
+                  </span>
+                  <span>Original: {(product.price + 69).toFixed(2)}</span>
+                </div>
+              </div>
+
+              {/* TITLE */}
+              <p className="text-center text-sm semi-bold">{product.title}</p>
+            </Link>
+          );
+        })}
+      </div>
+    </Page>
+  );
+}
+
+Products.propTypes = {
+  products: PropTypes.array,
+};
+
+export default Products;
--- app/pages/Homepage.js
+++ app/pages/Homepage.js
@@ -1,25 +1,51 @@
 import React, { useEffect } from "react";
 import Page from "../components/shared/Page";
 import Axios from "axios";
+import { useImmer } from "use-immer";
+import Products from "../components/shared/Products";
 
 function Homepage() {
+  const [products, setProducts] = useImmer({
+    isLoading: false,
+    feed: [],
+  });
+
   useEffect(() => {
     const request = Axios.CancelToken.source();
-    (async function getProducts() {
-      const response = await Axios.get(
-        "https://fakestoreapi.com/products?limit=5",
-        {
-          cancelToken: request.token,
-        }
-      );
-      console.log(response.data);
-    })();
+    try {
+      setProducts((draft) => {
+        draft.isLoading = true;
+      });
+
+      (async function getProducts() {
+        const response = await Axios.get(
+          "https://fakestoreapi.com/products?limit=5",
+          {
+            cancelToken: request.token,
+          }
+        );
+
+        setProducts((draft) => {
+          draft.isLoading = false;
+          draft.feed = response.data;
+        });
+      })();
+    } catch (error) {
+      console.log(error.message);
+    }
     return () => request.cancel();
   }, []);
 
+  if (products.isLoading) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <Page title="Harnak Spices: Online Shopping for Spices">
-      <h1>HOMEPAGE</h1>
+      <h1 className="py-10 text-center">
+        Welcome to Harnak Spices Online Store
+      </h1>
+      <Products products={products.feed} />
     </Page>
   );
 }
