005070cedf9519a03593664831bc8bfa93c3b72a
Adamu M. Dankore <zimmazone@yahoo.com>
2020-07-21 07:17:34 -0500

    Update file

--- app/components/shared/Header.js
+++ app/components/shared/Header.js
@@ -3,13 +3,13 @@ import { Link } from "react-router-dom";
 
 function Header() {
   return (
-    <div className="w-full bg-black py-1">
+    <div className="w-full bg-white py-1">
       <div className="mx-auto shaodow-xl flex justify-between items-center px-4">
         <Link to="/" className="flex items-center">
-          <img className="w-32 mr-2" src="./images/logo.png" />
+          <img className="w-32 mr-2" src="./images/logo2.png" />
         </Link>
         <input className="pl-2 py-1" type="text" placeholder="Search..." />
-        <div className="flex items-center text-white">
+        <div className="flex items-center">
           <div className="mr-3">
             Call, Text, WhatsApp me @ +234 803 404 2781
           </div>
