3bc737bb559003243779fe2e3ed5a1169248a960
Adamu Muhammad Dankore <zimmazone@yahoo.com>
2020-07-22 04:10:42 +0000

    update file

--- app/components/shared/NavBar.js
+++ app/components/shared/NavBar.js
@@ -1,6 +1,7 @@
 import React, { useContext } from 'react';
 import DispatchContext from '../../contextsProviders/DispatchContext';
 import StateContext from '../../contextsProviders/StateContext';
+import { Link } from 'react-router-dom';
 
 function Navbar() {
   const appDispatch = useContext(DispatchContext);
@@ -8,9 +9,9 @@ function Navbar() {
 
   return (
     <nav className="bg-white relative">
-      <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
+      <div className="max-w-7xl mx-auto px-2 lg:px-6 lg:px-8">
         <div className="relative flex items-center justify-between h-16">
-          <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+          <div className="absolute inset-y-0 left-0 flex items-center lg:hidden">
             {/* <!-- Mobile menu button--> */}
             <button
               className="inline-flex items-center justify-center p-2 rounded-md focus:outline-none transition duration-150 ease-in-out"
@@ -52,8 +53,9 @@ function Navbar() {
               )}
             </button>
           </div>
-          <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
-            <div className="flex-shrink-0">
+          <div className="flex-1 flex items-center justify-center lg:items-stretch lg:justify-start">
+            <Link to="/" className="flex-shrink-0">
+              {/* <span className="block lg:hidden">Menu</span> */}
               <img
                 className="block lg:hidden h-8 w-auto"
                 src="./images/logo2.png"
@@ -64,9 +66,9 @@ function Navbar() {
                 src="./images/logo2.png"
                 alt="Harnak Spices logo"
               />
-            </div>
+            </Link>
 
-            <div className="hidden sm:block sm:mx-auto">
+            <div className="hidden lg:block lg:mx-auto">
               <div className="flex">
                 <div className="mr-3 flex items-center">
                   <i className="text-4xl text-green-500 mr-2 fab fa-whatsapp-square"></i>
@@ -84,12 +86,12 @@ function Navbar() {
             </div>
           </div>
 
-          <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+          <div className="absolute inset-y-0 right-0 flex items-center pr-2 lg:static lg:inset-auto lg:ml-6 lg:pr-0">
             {/* <!-- Profile dropdown --> */}
             <div className="ml-3 relative">
               <div>
                 <button
-                  className="flex text-sm border-2 border-transparent rounded-full focus:outline-none focus:border-white transition duration-150 ease-in-out"
+                  className="flex text-lg border-2 border-transparent rounded-full focus:outline-none focus:border-white transition duration-150 ease-in-out"
                   id="user-menu"
                   aria-label="User menu"
                   aria-haspopup="true"
@@ -108,21 +110,21 @@ function Navbar() {
                   >
                     <a
                       href="#"
-                      className="block px-4 py-2 text-sm leading-5 text-gray-700 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 transition duration-150 ease-in-out"
+                      className="block px-4 py-2 text-lg leading-5 text-gray-700 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 transition duration-150 ease-in-out"
                       role="menuitem"
                     >
                       Your Profile
                     </a>
                     <a
                       href="#"
-                      className="block px-4 py-2 text-sm leading-5 text-gray-700 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 transition duration-150 ease-in-out"
+                      className="block px-4 py-2 text-lg leading-5 text-gray-700 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 transition duration-150 ease-in-out"
                       role="menuitem"
                     >
                       Settings
                     </a>
                     <a
                       href="#"
-                      className="block px-4 py-2 text-sm leading-5 text-gray-700 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 transition duration-150 ease-in-out"
+                      className="block px-4 py-2 text-lg leading-5 text-gray-700 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 transition duration-150 ease-in-out"
                       role="menuitem"
                     >
                       Sign out
@@ -136,7 +138,7 @@ function Navbar() {
       </div>
 
       {appState.toggleHamburgerMenu && (
-        <div className="block sm:hidden absolute bg-white inset-x-0 h-screen">
+        <div className="block lg:hidden absolute bg-white inset-x-0 h-screen">
           <div className="px-2 pt-2 pb-3">
             <div className="mb-5 flex items-center">
               <i className="text-4xl text-green-500 mr-2 fab fa-whatsapp-square"></i>
