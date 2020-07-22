b3ed0e1d6740a0587691c2ac88715a841a463bf2
Adamu Muhammad Dankore <zimmazone@yahoo.com>
2020-07-22 01:19:56 +0000

    header logos

--- app/components/shared/Header.js
+++ app/components/shared/Header.js
@@ -9,9 +9,18 @@ function Header() {
           <img className="w-32 mr-2" src="./images/logo2.png" />
         </Link>
 
-        <div className="flex items-center">
-          <div className="mr-3">
-            Call, Text, WhatsApp me @ +234 803 404 2781
+        <div className="flex items-center ">
+          <div className="mr-3 flex items-center">
+            <i className="text-4xl text-green-500 mr-2 fab fa-whatsapp-square"></i>
+            <span>Order on WhatsApp</span>
+          </div>
+          <div className="mr-3 flex items-center">
+            <i className="text-4xl text-blue-500 mr-2 fab fa-facebook-square"></i>
+            <span>Order on Facebook</span>
+          </div>
+          <div className="mr-3 flex items-center">
+            <i className="text-4xl mr-2 fas fa-phone-square"></i>
+            <span>Order by Call: +234 803 404 2781</span>
           </div>
         </div>
       </div>
