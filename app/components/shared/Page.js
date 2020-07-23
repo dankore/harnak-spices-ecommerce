03e03857230bc27203bce3bb89bb17e6a6afaadf
Adamu M. Dankore <zimmazone@yahoo.com>
2020-07-23 08:49:30 -0500

    Helmet

--- app/components/shared/Page.js
+++ app/components/shared/Page.js
@@ -1,19 +1,54 @@
 import React, { useEffect } from 'react';
 import Container from './Container';
 import PropTypes from 'prop-types';
+import { Helmet } from 'react-helmet';
 
 function Page(props) {
   useEffect(() => {
     window.scrollTo(0, 0);
-    document.title = props.title;
   }, [props.title]);
 
-  return <Container>{props.children}</Container>;
+  function formatTitleAndDescription(s) {
+    if (s) {
+      const inputToArray = s.split(' ');
+      if (inputToArray.length < 6) {
+        return `${inputToArray.slice(0, 5).join(' ')}`;
+      }
+      return `${inputToArray.slice(0, 5).join(' ')}...`;
+    }
+  }
+
+  return (
+    <Container>
+      <Helmet>
+        {/* General tags */}
+        <title>{`${props.title} | Harnak Spices`}</title>
+        <meta name="description" content={formatTitleAndDescription(props.description)} />
+        <meta name="image" content={props.image} />
+
+        {/* OpenGraph tags */}
+        <meta property="og:url" content={props.url} />
+        <meta property="og:title" content={props.title} />
+        <meta property="og:description" content={formatTitleAndDescription(props.description)} />
+        <meta property="og:image" content={props.image} />
+
+        {/* Twitter Card tags */}
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content={props.title} />
+        <meta name="twitter:description" content={formatTitleAndDescription(props.description)} />
+        <meta name="twitter:image" content={props.image} />
+      </Helmet>
+      {props.children}
+    </Container>
+  );
 }
 
 Page.propTypes = {
   children: PropTypes.node.isRequired,
   title: PropTypes.any,
+  description: PropTypes.any,
+  image: PropTypes.any,
+  url: PropTypes.any,
 };
 
 export default Page;
---
+++ app/components/shared/SeoDefault.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { Helmet } from 'react-helmet';
+
+function SeoDefault() {
+  const image = `https://res.cloudinary.com/my-nigerian-projects/image/upload/v1594491219/free-background-press-v2_pg66nf.svg`;
+  const description = `Find the best African spices online.`;
+  const title = `Shop online for your spices | Harnak Spices`;
+
+  function formatTitleAndDescription(s) {
+    if (s) {
+      const inputToArray = s.split(' ');
+      if (inputToArray.length < 6) {
+        return `${inputToArray.slice(0, 5).join(' ')}`;
+      }
+      return `${inputToArray.slice(0, 5).join(' ')}...`;
+    }
+  }
+  return (
+    <>
+      <Helmet>
+        {/* General tags */}
+        <title>{title}</title>
+        <meta name="description" content={formatTitleAndDescription(description)} />
+        <meta name="image" content={image} />
+
+        {/* OpenGraph tags */}
+        <meta property="og:url" content={window.location} />
+        <meta property="og:title" content={title} />
+        <meta property="og:description" content={formatTitleAndDescription(description)} />
+        <meta property="og:image" content={image} />
+
+        {/* Twitter Card tags */}
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content={title} />
+        <meta name="twitter:description" content={formatTitleAndDescription(description)} />
+        <meta name="twitter:image" content={image} />
+      </Helmet>
+    </>
+  );
+}
+
+export default SeoDefault;
