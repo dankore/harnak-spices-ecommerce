93227ec4b3b6a2c37d0ae5b9389185e314969ae8
Adamu Muhammad Dankore <zimmazone@yahoo.com>
2020-07-23 09:04:07 +0000

    rm min h

--- app/components/shared/Container.js
+++ app/components/shared/Container.js
@@ -2,11 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 function Container(props) {
-  return (
-    <div style={{ minHeight: 500 + 'px' }} className="bg-white px-3">
-      {props.children}
-    </div>
-  );
+  return <div className="bg-white px-3">{props.children}</div>;
 }
 
 Container.propTypes = {
