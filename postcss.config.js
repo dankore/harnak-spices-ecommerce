6e1953a08da2bcf5ccbcd005ec095e3c58944b7d
Adamu M. Dankore <zimmazone@yahoo.com>
2020-07-24 23:58:45 -0500

    update file

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,6 @@
 const PurgeCss = require('@fullhuman/postcss-purgecss');
 const PurgeOptions = {
-  // Specify the paths to all of the template files in your project
+  // SPECIFY PATH THAT CONTAINS CSS
   content: ['./app/**/*.html', './app/**/*.js'],
 
   // Include any special characters you're using in this regular expression
