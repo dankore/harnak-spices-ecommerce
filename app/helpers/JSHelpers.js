93d4624ea6343a23666a292e7bd10af66be724b1
Adamu M. Dankore <zimmazone@yahoo.com>
2020-07-26 17:11:42 -0500

    basket update

--- app/helpers/JSHelpers.js
+++ app/helpers/JSHelpers.js
@@ -8,7 +8,7 @@ module.exports = {
       return `${inputToArray.slice(0, 5).join(' ')}...`;
     }
   },
-  removeDuplicates: function (array) {
+  removeDuplicatesGetCount: function (array) {
     const count = {};
     const result = [];
 
@@ -37,30 +37,4 @@ module.exports = {
 
     return { count, result };
   },
-  countEachObjectInArray: function (array) {
-    const tempStorage = {};
-
-    for (let i = 0; i < array.length; i++) {
-      const element = array[i];
-      /**
-       * @param element
-       * {
-        category: 'men clothing',
-        description: 'Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday',
-        id: 1,
-        image: 'https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg',
-        price: 109.95,
-        title: 'Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops'
-        }
-       */
-
-      if (!tempStorage.hasOwnProperty(element.id)) {
-        tempStorage[element.id] = 1;
-      } else {
-        tempStorage[element.id] += 1;
-      }
-    }
-
-    return tempStorage;
-  }
 };
--- app/pages/BasketPage.js
+++ app/pages/BasketPage.js
@@ -1,18 +1,34 @@
 import React, { useContext } from "react";
 import Page from '../components/shared/Page';
 import StateContext from '../contextsProviders/StateContext';
-import { removeDuplicates } from '../helpers/JSHelpers';
+import { removeDuplicatesGetCount } from '../helpers/JSHelpers';
+import { useImmer } from 'use-immer';
 
 function BasketPage() {
   const appState = useContext(StateContext);
+  const [state, seState] = useImmer({});
+
+  function handleChange(e) {
+    console.log(e.target.value);
+  }
+
   return (
     <Page>
       <h1>Your Basket</h1>
-      {removeDuplicates(appState.basket).result.map((item, index) => {
+      {removeDuplicatesGetCount(appState.basket).result.map((item, index) => {
         return (
           <div key={index}>
             <h1>{item.title}</h1>
-            <span>Count: {removeDuplicates(appState.basket).count[item.id]}</span>
+            <select className='bg-gray-100 px-2 py-1 border border-gray-400'>
+              <option>{removeDuplicatesGetCount(appState.basket).count[item.id]}</option>
+              <option>2</option>
+              <option>3</option>
+              <option>4</option>
+              <option>5</option>
+              <option>6</option>
+              <option>7</option>
+              <option>8</option>
+            </select>
           </div>
         );
       })}
