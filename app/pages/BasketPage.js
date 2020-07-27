dd9bdd6c11dab65ec13f3f27b50675d14c95e107
Adamu M. Dankore <zimmazone@yahoo.com>
2020-07-27 02:12:50 -0500

    update basket

--- app/pages/BasketPage.js
+++ app/pages/BasketPage.js
@@ -31,7 +31,7 @@ function BasketPage() {
   }
 
   function removeItemsFromLocalStorage(item, count) {
-    count = Math.abs(count);
+    count = Math.abs(count); // BCAUSE @COUNT IS NEGATIVE
     while (count > 0) {
       localStorage.deleteItem('basket', item);
       count--;
@@ -40,32 +40,60 @@ function BasketPage() {
 
   return (
     <Page title="Basket">
-      <h1>Your Basket</h1>
-      {removeDuplicatesGetCount(appState.basket).result.map((item, index) => {
-        return (
-          <div key={index}>
-            <h1>{item.title}</h1>
-            <select
-              data-item={`${JSON.stringify(item)}`}
-              data-previousval={`${removeDuplicatesGetCount(appState.basket).count[item.id]}`}
-              onChange={handleChange}
-              className="bg-gray-100 px-2 py-1 border border-gray-400"
-            >
-              <option>{removeDuplicatesGetCount(appState.basket).count[item.id]}</option>
-              <option>1</option>
-              <option>2</option>
-              <option>3</option>
-              <option>4</option>
-              <option>5</option>
-              <option>6</option>
-              <option>7</option>
-              <option>8</option>
-              <option>9</option>
-              <option>10</option>
-            </select>
-          </div>
-        );
-      })}
+      <div className='max-w-6xl mx-auto lg:flex pt-12 pb-3'>
+        <div className=''>
+          {removeDuplicatesGetCount(appState.basket).result.map((item, index) => {
+            return (
+              <div className='grid grid-cols-4 gap-3' key={index}>
+                
+                {/* IMAGE */}
+                <img
+                  onClick={() => appDispatch({ type: 'toggleImageViewer' })}
+                  className="flex w-56 h-56 mb-2 cursor-pointer"
+                  src={item.image}
+                />
+
+                {/* TITLE */}
+                <p  className=''>{item.title}</p>
+
+                {/* SELECT */}
+                <div className='text-center'>
+                  <select
+                    data-item={`${JSON.stringify(item)}`}
+                    data-previousval={`${removeDuplicatesGetCount(appState.basket).count[item.id]}`}
+                    onChange={handleChange}
+                    className="flex-none bg-gray-100 px-2 py-1 border border-gray-400"
+                  >
+                    <option>{removeDuplicatesGetCount(appState.basket).count[item.id]}</option>
+                    <option>1</option>
+                    <option>2</option>
+                    <option>3</option>
+                    <option>4</option>
+                    <option>5</option>
+                    <option>6</option>
+                    <option>7</option>
+                    <option>8</option>
+                    <option>9</option>
+                    <option>10</option>
+                  </select>
+                </div>
+
+                {/* PRICE */}
+                <div className="flex justify-center">
+                  <div>
+                    <span className="text-red-600 block">
+                      <span className="text-sm">Sale</span>{' '}
+                      <span className="text-2xl font-bold">{item.price}</span>
+                    </span>
+                  </div>
+                </div>
+
+              </div>
+            );
+          })}
+        </div>
+        <div style={{minWidth: 400+'px'}} className='bg-green-400'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corr</div>
+      </div>
     </Page>
   );
 }
