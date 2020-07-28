144c022ad6603f218384af3e52960b541eefa0e3
Adamu Muhammad Dankore <45286534+dankore@users.noreply.github.com>
2020-07-28 04:07:55 +0000

    Update ViewSingleProduct.js

--- app/pages/ViewSingleProduct.js
+++ app/pages/ViewSingleProduct.js
@@ -1,44 +1,14 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import Page from '../components/shared/Page';
-import Axios from 'axios';
-import { useImmer } from 'use-immer';
 import { useParams } from 'react-router-dom';
 import SingleProductHtml from '../components/products/SingleProductHtml';
-import LoadingDotsAnimation from '../components/shared/LoadingDotsAnimation';
+import { data } from '../data';
 
 function ViewSingleProduct() {
   const { id } = useParams();
-
-  const [singleProduct, setSingleProduct] = useImmer({
-    isLoading: false,
-    feed: [],
-  });
-
-  useEffect(() => {
-    const request = Axios.CancelToken.source();
-    try {
-      setSingleProduct((draft) => {
-        draft.isLoading = true;
-      });
-      (async function getSingleProduct() {
-        const response = await Axios.get(`https://fakestoreapi.com/products/${id}`, {
-          cancelToken: request.token,
-        });
-
-        setSingleProduct((draft) => {
-          draft.isLoading = false;
-          draft.feed = response.data;
-        });
-      })();
-    } catch (error) {
-      console.log(error.message);
-    }
-    return () => request.cancel();
-  }, []);
-
-  if (singleProduct.isLoading) {
-    return <LoadingDotsAnimation />;
-  }
+  let singleProduct = {
+    feed: data[id],
+  };
 
   const descriptionSEO = `SALE ${(singleProduct.feed.price + 69).toFixed(2)}! Price before sale: ${
     singleProduct.feed.price
