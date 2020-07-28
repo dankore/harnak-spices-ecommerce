1c2fd25b997e5bc5f2f3e06369e4db81877358bf
Adamu M. Dankore <zimmazone@yahoo.com>
2020-07-28 10:03:21 -0500

    Refac layouts

--- app/components/products/Products.js
+++ app/components/products/Products.js
@@ -4,31 +4,40 @@ import { Link } from 'react-router-dom';
 
 function Products({ products }) {
   return (
-    <div className="bg-white flex justify-around flex-wrap">
-      {products.map((product) => {
-        return (
-          <Link to={`/product/${product.id}`} className="w-56 my-10 lg:m-3" key={product.id}>
-            <img className="w-56 h-56 mb-2" src={product.image} />
+    <>
+      {products.length > 0 ? (
+        <div className="bg-white grid grid-cols-1 lg:grid-cols-3 lg:gap-4 lg:px-3">
+          {products.map((product) => {
+            return (
+              <Link to={`/product/${product.id}`} className="border mb-5 lg:mb-0" key={product.id}>
+                <img className="object-cover h-64 w-full cursor-pointer" src={product.image} />
 
-            {/* PRICE */}
-            <div className="flex justify-center">
-              <div>
-                <span className="text-red-600 block">
-                  <span className="text-sm">Sale</span>{' '}
-                  <span className="text-2xl font-bold">{product.price}</span>
-                </span>
-                <span className="text-sm text-gray-600 -mt-2 block">
-                  Original: {(product.price + 69).toFixed(2)}
-                </span>
-              </div>
-            </div>
-
-            {/* TITLE */}
-            <p className="text-center text-sm semi-bold mt-3">{product.title}</p>
-          </Link>
-        );
-      })}
-    </div>
+                <div className="flex items-center justify-between px-2">
+                  {/* TITLE */}
+                  <p className="text-center text-sm semi-bold">{product.title}</p>
+                  {/* PRICE */}
+                  <div className="flex justify-center">
+                    <div>
+                      <span className="text-red-600 block">
+                        <span className="text-sm">Sale</span>{' '}
+                        <span className="text-2xl font-bold">{product.price}</span>
+                      </span>
+                      <span className="text-sm text-gray-600 -mt-2 block">
+                        Original: {(product.price + 69).toFixed(2)}
+                      </span>
+                    </div>
+                  </div>
+                </div>
+              </Link>
+            );
+          })}
+        </div>
+      ) : (
+        <div className="text-2xl text-center">
+          No Products at this time. Please check back later.
+        </div>
+      )}
+    </>
   );
 }
 
