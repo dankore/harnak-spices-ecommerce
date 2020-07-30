be3428d1f9a17bda79dfc8db9ee0026bf20247de
Adamu Muhammad Dankore <zimmazone@yahoo.com>
2020-07-30 07:13:34 +0000

    update file

--- app/components/shared/ImageViewer.js
+++ app/components/shared/ImageViewer.js
@@ -3,9 +3,9 @@ import PropTypes from 'prop-types';
 
 function ImageViewer({ image, title }) {
   return (
-    <div className="modal shadow-lg absolute bg-white">
-      <img src={image} alt="product" />
-      {title}
+    <div style={{ height: 550 + 'px' }} className="modal shadow-lg absolute bg-white">
+      <img style={{ height: 500 + 'px' }} src={image} alt="product" />
+      <p className="px-3 w-64 text-center">{title}</p>
     </div>
   );
 }
