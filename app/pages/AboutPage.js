f8f712198dca8c65dbf5d6e32dbe2f8277aa46c3
Adamu M. Dankore <zimmazone@yahoo.com>
2020-09-06 00:41:11 -0500

    About page misc

--- app/pages/AboutPage.js
+++ app/pages/AboutPage.js
@@ -1,27 +1,30 @@
 import React from 'react';
+import Page from '../components/shared/Page';
 
 function AboutPage() {
   return (
-    <div className="mt-24">
-      <h1 className="text-center">About Harnak Spices</h1>
-      <div className="flex flex-wrap justify-center">
-        <img
-          className="custom-shadow m-3"
-          style={{ height: 400 + 'px' }}
-          src="https://res.cloudinary.com/my-nigerian-projects/image/upload/v1599369307/har/1_rud7fj.jpg"
-        />
-        <img
-          className="custom-shadow m-3"
-          style={{ height: 400 + 'px' }}
-          src="https://res.cloudinary.com/my-nigerian-projects/image/upload/v1599369307/har/2_oyldgo.jpg"
-        />
-        <img
-          className="custom-shadow m-3"
-          style={{ height: 400 + 'px' }}
-          src="https://res.cloudinary.com/my-nigerian-projects/image/upload/v1599369307/har/3_n0w7r6.jpg"
-        />
+    <Page title="About Harnak Spices">
+      <div className="mt-24">
+        <h1 className="text-center">About Harnak Spices</h1>
+        <div className="flex flex-wrap justify-center">
+          <img
+            className="custom-shadow m-3"
+            style={{ height: 400 + 'px' }}
+            src="https://res.cloudinary.com/my-nigerian-projects/image/upload/v1599369307/har/1_rud7fj.jpg"
+          />
+          <img
+            className="custom-shadow m-3"
+            style={{ height: 400 + 'px' }}
+            src="https://res.cloudinary.com/my-nigerian-projects/image/upload/v1599369307/har/2_oyldgo.jpg"
+          />
+          <img
+            className="custom-shadow m-3"
+            style={{ height: 400 + 'px' }}
+            src="https://res.cloudinary.com/my-nigerian-projects/image/upload/v1599369307/har/3_n0w7r6.jpg"
+          />
+        </div>
       </div>
-    </div>
+    </Page>
   );
 }
 
