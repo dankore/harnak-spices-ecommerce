5285b1da06cde57c5452bb65ff550a38e5cd3b12
Adamu M. Dankore <zimmazone@yahoo.com>
2020-09-07 11:29:25 -0500

    Branch page enhancement

--- app/components/shared/Branch.js
+++ app/components/shared/Branch.js
@@ -7,12 +7,14 @@ function Branch({ state, phone, background }) {
       style={{ backgroundImage: `url(${background})` }}
       className="bg-cover bg-center flex items-center justify-center custom-shadow w-full h-48 sm:w-56 sm:h-48 my-3 sm:m-3"
     >
-      <div className="font-bold bg-white opacity-75 hover:opacity-100">
-        <p className="text-center ">{state}</p>
-        <a href={`tel:${phone}`} className="flex py-1 px-2 items-center hover:text-red-600">
-          <i className="text-3xl text-gray-700 mr-2 fas fa-phone-square"></i>
-          <span>{phone}</span>
-        </a>
+      <div className="absolute font-bold w-full h-48 sm:w-56 sm:h-48 bg-blue-900 opacity-75 hover:bg-transparent flex justify-center items-center text-white">
+        <div className="hover:bg-black">
+          <p className="text-center">{state}</p>
+          <a href={`tel:${phone}`} className="flex py-1 px-2 items-center">
+            <i className="text-3xl mr-2 fas fa-phone-square"></i>
+            <span>{phone}</span>
+          </a>
+        </div>
       </div>
     </div>
   );
