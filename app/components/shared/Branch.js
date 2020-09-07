a6368fa221024ea6d6502766f32fdab04b8850d5
Adamu M. Dankore <zimmazone@yahoo.com>
2020-09-07 02:23:24 -0500

    Branch page enhancement

--- app/components/shared/Branch.js
+++ app/components/shared/Branch.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-function Branch({ state, phone }) {
+function Branch({ state, phone, background }) {
   return (
-    <div className="flex items-center justify-center custom-shadow w-full h-48 sm:w-56 sm:h-48 my-3 sm:m-3">
-      <div>
-        <p className="text-center font-bold">{state}</p>
+    <div
+      style={{ backgroundImage: `url(${background})` }}
+      className="bg-cover bg-center flex items-center justify-center custom-shadow w-full h-48 sm:w-56 sm:h-48 my-3 sm:m-3"
+    >
+      <div className="font-bold">
+        <p className="text-center ">{state}</p>
         <a
           href={`tel:${phone}`}
-          className="flex py-1 px-2 items-center active:bg-gray-300 hover:bg-gray-200"
+          className="flex py-1 px-2 items-center bg-white opacity-75 active:bg-gray-300 hover:bg-gray-200"
         >
           <i className="text-3xl text-gray-700 mr-2 fas fa-phone-square"></i>
           <span>{phone}</span>
@@ -21,6 +24,7 @@ function Branch({ state, phone }) {
 Branch.propTypes = {
   state: PropTypes.string,
   phone: PropTypes.string,
+  background: PropTypes.string,
 };
 
 export default Branch;
--- app/components/shared/Branches.js
+++ app/components/shared/Branches.js
@@ -6,12 +6,36 @@ function Branches() {
     <div className="">
       <h1 className="text-center">Our Branches</h1>
       <div className="flex flex-wrap justify-center">
-        <Branch state="ABUJA, F.C.T" phone="+234(0)803 404 2781" />
-        <Branch state="GOMBE" phone="+234(0)907 716 1025" />
-        <Branch state="KANO" phone="+234(0)810 355 1496" />
-        <Branch state="KEBBI" phone="+234(0)703 222 6508" />
-        <Branch state="NIGER" phone="+234(0)807 625 5816" />
-        <Branch state="SOKOTO" phone="+234(0)803 720 7898" />
+        <Branch
+          state="ABUJA, F.C.T"
+          phone="+234(0)803 404 2781"
+          background="https://res.cloudinary.com/my-nigerian-projects/image/upload/v1599461483/har/abuja_city_gate_f1oxhp.jpg"
+        />
+        <Branch
+          state="GOMBE"
+          phone="+234(0)907 716 1025"
+          background="https://res.cloudinary.com/my-nigerian-projects/image/upload/v1599462653/har/Gombe-State_cnjuiu.jpg"
+        />
+        <Branch
+          state="KANO"
+          phone="+234(0)810 355 1496"
+          background="https://res.cloudinary.com/my-nigerian-projects/image/upload/v1599462304/har/kano_vurwiz.jpg"
+        />
+        <Branch
+          state="KEBBI"
+          phone="+234(0)703 222 6508"
+          background="https://res.cloudinary.com/my-nigerian-projects/image/upload/v1599462861/har/kebbi_vbimfg.jpg"
+        />
+        <Branch
+          state="NIGER"
+          phone="+234(0)807 625 5816"
+          background="https://res.cloudinary.com/my-nigerian-projects/image/upload/v1599462927/har/Zuma_Rock_wzms6d.jpg"
+        />
+        <Branch
+          state="SOKOTO"
+          phone="+234(0)803 720 7898"
+          background="https://res.cloudinary.com/my-nigerian-projects/image/upload/v1599463028/har/sokoto_h31mxg.jpg"
+        />
       </div>
     </div>
   );
